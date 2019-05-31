ee85a0a7e4ec411540088a8448f6be021bc737d2
nehemiaslux <eitanlux@gmail.com>
2019-05-31 12:35:26 -0600

    jjJ

--- js/script.js
+++ js/script.js
@@ -1,11 +1,16 @@
 var pokemon= angular.module("pokemon",[])
 
-pokemon.controller("pokedex", function ($scope,$rootScope,$http){
-	$http({
-		method: "GET",
-		url: "https://pokeapi.co/api/v2/pokemon/25/"
+pokemon.controller("pokedexCtrl", function ($scope,$rootScope,$http){
+	$scope.pokedex = [];
 
-	}).then(function callbackSucess(y){
-		console.log(y);
-	})
+	for(var i = 0; i < 100; i++){
+
+		$http({
+			method: "GET",
+			url: "https://pokeapi.co/api/v2/pokemon/25/"+i
+
+		}).then(function callbackSucess(y){
+		$scope.pokedex.push(y);
+		})
+	}
 })
